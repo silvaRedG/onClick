c0f0afdef92dac24c5790b5da65b1d24444efd84
silvaRedG <136278969+silvaRedG@users.noreply.github.com>
2023-11-17 08:12:41 -0300

    Add files via upload

---
+++ src/global/globalStyles.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+
+
+body{
+
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: none;
+}
+
+
+
+
+`
+
+export default GlobalStyles
